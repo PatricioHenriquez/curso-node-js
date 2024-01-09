1b551ac1307d325d5d5fb553f22dfc4cf02e5e44
PatoDev <patricio.henriquez.a@gmail.com>
2024-01-09 15:30:40 -0300

    15:31

--- 10.free-port.js
+++ 10.free-port.js
@@ -14,7 +14,7 @@ function findAvailablePort(desiredPort){
         })
         
         server.on('error', (err) => {
-            if (err.code == 'EADDINUSE') {
+            if (err.code == 'EADDRINUSE') {
                 findAvailablePort(0).then(port => resolve(port))
             } else{
                 reject(err)
